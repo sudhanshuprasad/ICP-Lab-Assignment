1c454d2abdaa6e0b11c1cef8051ff94869c9e2db
Sudhanshu Prasad <sudhanshuprasad.sp@gmail.com>
2020-12-17 22:31:31 +0530

    q2

---
+++ Assignment1/src/Q2.java
@@ -0,0 +1,16 @@
+
+public class Q2 {
+	public static void main(String[] args) {
+		System.out.println("+---------------------------------------------------------------+");
+		System.out.println("|                                                        ####   |");
+		System.out.println("|                                                        ####   |");
+		System.out.println("|                                                        ####   |");
+		System.out.println("|                                                        ####   |");
+		System.out.println("|                                                               |");
+		System.out.println("|                              Sudhanshu Prasad                 |");
+		System.out.println("|                              Ramgarh,                         |");
+		System.out.println("|                              Jharkhand                        |");
+		System.out.println("|                              825101                           |");
+		System.out.println("+---------------------------------------------------------------+");
+	}
+}
