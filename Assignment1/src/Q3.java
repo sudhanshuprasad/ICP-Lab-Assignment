7156f54af55623537e4e2e0e0acdd89a97ce5837
Sudhanshu Prasad <sudhanshuprasad.sp@gmail.com>
2020-12-18 11:49:23 +0530

    q3 completed

---
+++ Assignment1/src/Q3.java
@@ -0,0 +1,16 @@
+
+public class Q3 {
+	public static void main(String[] args) {
+		System.out.println("      ssssssssssssss             pppppppppppp                    ");
+		System.out.println("      s                          p           p                   ");
+		System.out.println("      s                          p            p                  ");
+		System.out.println("      s                          p            p                  ");
+		System.out.println("      s                          p           p                   ");
+		System.out.println("      ssssssssssssss             pppppppppppp                    ");
+		System.out.println("                   s             p                               ");
+		System.out.println("                   s             p                               ");
+		System.out.println("                   s             p                               ");
+		System.out.println("                   s             p                               ");
+		System.out.println("      ssssssssssssss             p                               ");
+	}
+}
